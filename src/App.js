bb43385f0d1d0cefe750dccd47c95b24b8f7fdda
chandrasekharnagella <120458828+chandrasekharnagella@users.noreply.github.com>
2023-01-06 17:23:25 +0530

    Add files via upload
    Fetch Data

---
+++ src/App.js
@@ -0,0 +1,17 @@
+ import React, { useEffect } from 'react';
+import './App.css';
+import { Data } from './components/data';
+
+function App() {
+
+
+  return (
+    <div className="App">
+     
+        <Data/>
+
+    </div>
+  );
+}
+
+export default App;
